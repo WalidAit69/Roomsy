1bc7e7298ed131f30c530a7a0a3a0e490646f564
WalidAit69 <aitwalid2000@gmail.com>
2023-10-18 22:52:07 +0100

    testing

--- app/src/widgets/Place Widgets/PlaceCardSkeleton.jsx
+++ app/src/widgets/Place Widgets/PlaceCardSkeleton.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import "../../components/Places Components/PlacePage.css";
 
 
 function PlaceCardSkeleton({ cards }) {
