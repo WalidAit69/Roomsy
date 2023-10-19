acad993bfe96c763f0c4b51b13dd5b19cc5b2cf3
WalidAit69 <aitwalid2000@gmail.com>
2023-10-19 11:49:22 +0100

    update

--- server/index.js
+++ server/index.js
@@ -5,28 +5,38 @@ import router from "./routes/route.js";
 import placerouter from "./routes/Placeroute.js";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-import uploadMiddleware from "./uploadMiddleware.cjs"
+import uploadMiddleware from "./uploadMiddleware.cjs";
 import uploadbylinkMiddleware from "./uploadbylinkMiddleware.cjs";
-
 
 const app = express();
 
-
-
 app.use(express.json());
-app.use(cors({credentials:true,origin:'https://roomsy-2.vercel.app/'}));
+app.use(cors({ credentials: true, origin: "https://roomsy-2.vercel.app/" }));
 app.disable("x-powered-by");
 app.use(morgan("tiny"));
 app.use(cookieParser());
-app.use('/uploads' , uploadMiddleware);
-app.use('/server/routes/uploads' , uploadbylinkMiddleware);
-
-
+app.use("/uploads", uploadMiddleware);
+app.use("/server/routes/uploads", uploadbylinkMiddleware);
 
 app.get("/", (req, res) => {
-    res.send("Home get Request");
+  res.send("Home get Request");
 });
 
-
-app.use('/api',router);
+app.use("/api", router);
 app.use(placerouter);
+
+const port = 3001;
+
+connectDB()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
+    } catch (error) {
+      console.log("cannot connect to the server");
+    }
+  })
+  .catch((error) => {
+    console.log("invalid database");
+  });
